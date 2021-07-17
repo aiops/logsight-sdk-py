15593bd1b944ce59b5213386b098f12d0f7f19ec
Jorge Cardoso <jorge.cardoso@huawei.com>
2021-07-17 11:53:41 +0200

    äpplication endpoint support

--- logsight/applications.py
+++ logsight/applications.py
@@ -3,17 +3,6 @@ import urllib.parse
 import html
 import json
 
-from logsight.template import Templates
-from logsight.incidents import Incidents
-
-
-ANOMALIES = {
-    "log_ad": Templates,
-    "incidents": Incidents,
-    # "log_ad": LogAd,
-    # "count_ad": CountAd,
-}
-
 
 class LogsightApplication:
 
@@ -30,8 +19,9 @@ class LogsightApplication:
         return self._post(data, self.path_create)
 
     def delete(self, app_id):
-        data = {'key': self.private_key}
-        return self._post(data, '/'.join([self.path_delete, app_id]))
+        data = {}
+        query = f'{app_id}?key={self.private_key}'
+        return self._post(data, '/'.join([self.path_delete, query]))
 
     def _post(self, data, path):
         try:
@@ -58,11 +48,11 @@ class LogsightApplication:
 
 if __name__ == '__main__':
     PRIVATE_KEY = 'q1oukwa2hzsoxg4j7arvd6q67ik'
-    APP_NAME = 'unittest_6'
+    APP_NAME = 'unittest_7'
 
     app = LogsightApplication(PRIVATE_KEY)
     app_id = app.create(APP_NAME)
     print(app_id)
-    result = app.delete(str(app_id['id']))
-    # result = app.delete(str(146))
-    print(result)
+    # result = app.delete(str(app_id['id']))
+    # result = app.delete(str(152))
+    # print(result)
