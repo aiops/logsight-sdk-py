9320ba75617f76a0e3262ef0bd634540ad83f913
Jorge Cardoso <jorge.cardoso@huawei.com>
2021-07-21 14:05:29 +0200

    Simplification of test

--- tests/test_app_mng.py
+++ tests/test_app_mng.py
@@ -15,17 +15,24 @@ class TestAppManagement(unittest.TestCase):
     def tearDownClass(cls):
         pass
 
-    def test_create_delete_app(self):
-        app_name = 'test_create_app'
+    def create_app(self, app_name):
         status_code, content = self.app_mng.create(app_name)
         self.assertEqual(status_code, 200)
         self.assertIsInstance(content, dict)
         self.assertIsInstance(content['id'], int)
+        return status_code, content
 
-        status_code, content = self.app_mng.delete(str(content['id']))
+    def delete_app(self, app_id):
+        status_code, content = self.app_mng.delete(app_id)
         self.assertEqual(status_code, 200)
         self.assertIsInstance(content, dict)
         self.assertTrue('description' in content)
+        return status_code, content
+
+    def test_create_delete_app(self):
+        app_name = 'test_create_app'
+        status_code, content = self.create_app(app_name)
+        self.delete_app(str(content['id']))
 
     def test_create_app_invalid_name(self):
         app_name = 'test-create-app'
@@ -35,16 +42,13 @@ class TestAppManagement(unittest.TestCase):
 
     def test_create_duplicate(self):
         app_name = 'test_create_app'
-        status_code, content = self.app_mng.create(app_name)
-        self.assertEqual(status_code, 200)
+        _, content = self.create_app(app_name)
 
         with self.assertRaises(SystemExit):
             status_code, content = self.app_mng.create(app_name)
             self.assertEqual(status_code, 400)
 
-        status_code, content = self.app_mng.delete(str(content['id']))
-        self.assertEqual(status_code, 200)
-        self.assertTrue('description' in content)
+        self.delete_app(str(content['id']))
 
     def test_delete_no_app(self):
         app_id = '0123456789'
@@ -52,21 +56,19 @@ class TestAppManagement(unittest.TestCase):
             status_code, content = self.app_mng.delete(app_id)
             self.assertEqual(status_code, 400)
 
-    def test_list_apps(self):
-        n = 3
+    def test_create_list_delete_apps(self):
+        n = 10
         app_names = [f'test_create_app_{i}' for i in range(n)]
-        app_ids = [self.app_mng.create(app_names[i])[1]['id'] for i in range(n)]
+        app_ids = [self.create_app(app_name)[1]['id'] for app_name in app_names]
 
         status_code, content = self.app_mng.lst()
         self.assertEqual(status_code, 200)
-        app_list = [(d['id'], d['name']) for d in content]
+        app_list = [(d['id'], d['name']) for d in content if 'test_create_app_' in d['name']]
 
         self.assertCountEqual(zip(app_ids, app_names), app_list)
 
         for _id in app_ids:
-            status_code, content = self.app_mng.delete(str(_id))
-            self.assertEqual(status_code, 200)
-            self.assertTrue('description' in content)
+            self.delete_app(str(_id))
 
     def test_list_apps_empty(self):
         status_code, content = self.app_mng.lst()
--- tests/test_single_app.py
+++ tests/test_single_app.py
@@ -1,11 +1,8 @@
-import sys
 import unittest
-import logging
 import time
 
 from integration.load_logs import LOG_FILES
 from integration.send_logs import SendLogs
-from logsight.logger import LogsightLogger
 from logsight.result import LogsightResult
 from config import PRIVATE_KEY, APP_NAME, DELAY_TO_QUERY_BACKEND
 from logsight.utils import now
@@ -23,37 +20,15 @@ class TestSingleApp(unittest.TestCase):
         cls.dt_start = now()
         print('Starting message sending', cls.dt_start)
 
-        logger, handler = cls.__setup_handler()
-
-        r = SendLogs(logger)
-        r.send_log_messages(log_file_name=LOG_FILES['hadoop'], n_messages=N_LOG_MESSAGES_TO_SEND)
-
-        cls.__remove_handler(logger, handler)
+        s = SendLogs(PRIVATE_KEY, APP_NAME)
+        s.send_log_messages(log_file_name=LOG_FILES['hadoop'], n_messages=N_LOG_MESSAGES_TO_SEND)
+        s.flush()
 
         cls.dt_end = now()
         print('Ended message sending', cls.dt_end)
 
         print('Sleeping before querying backend:', DELAY_TO_QUERY_BACKEND, 'sec')
         time.sleep(DELAY_TO_QUERY_BACKEND)
-
-    @staticmethod
-    def __setup_handler():
-        handler = LogsightLogger(PRIVATE_KEY, APP_NAME)
-        handler.setLevel(logging.DEBUG)
-
-        stdout_handler = logging.StreamHandler(sys.stdout)
-        stdout_handler.setLevel(logging.DEBUG)
-
-        logger = logging.getLogger(__name__)
-        logger.setLevel(logging.DEBUG)
-        logger.addHandler(handler)
-        # logger.addHandler(stdout_handler)
-        return logger, handler
-
-    @staticmethod
-    def __remove_handler(logger, handler):
-        handler.close()
-        logger.removeHandler(handler)
 
     def test_template_count(self):
         templates = LogsightResult(PRIVATE_KEY, APP_NAME)\
